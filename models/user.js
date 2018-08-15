469b0d0ede51a449f6806a8bd4d9f34eb89cc154
Isaac Pierce <contact@isaacdpierce.com>
2018-08-15 16:18:00 -0700

    Install and wire up bcrypt

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // Schema is used to tell mongoose about the particular fields the model will have
+const bcrypt = require('bcrypt-nodejs');
 
 // Set up instructions (data model) for mongoose to handle
 
@@ -8,6 +9,25 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
+});
+
+// On save hook -> encrypt password
+userSchema.pre('save', function(next) {
+  const user = this;
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 // Create the model class
