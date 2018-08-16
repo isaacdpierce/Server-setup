cf82308d07b73fa84567cf6a4351e70713124125
Isaac Pierce <contact@isaacdpierce.com>
2018-08-15 20:49:13 -0700

    Complete modular back end server with mongodb

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -8,6 +8,12 @@ function tokenForUser(user) {
   return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
 
+exports.signin = function(req, res, next) {
+  // User has already had email and password auth'd
+  // Supply token
+  res.send({ token: tokenForUser(req.user) });
+};
+
 exports.signup = function(req, res, next) {
   // pull data out of the request object when it is a post request with req.body
   const email = req.body.email;
--- services/passport.js
+++ services/passport.js
@@ -3,6 +3,39 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+// Create local Strategy
+const localOptions = { usernameField: 'email' };
+const localLogin = new LocalStrategy(localOptions, function(
+  email,
+  password,
+  done
+) {
+  // Verify username and password
+  // Call done on the user if they are correct
+  // Else call done with false
+  User.findOne({ email: email }, function(err, user) {
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      return done(null, false);
+    }
+
+    // Compare passwords -> is `password` === user.password
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) {
+        return done(err);
+      }
+      if (!isMatch) {
+        return done(null, false);
+      }
+
+      return done(null, user);
+    });
+  });
+});
 
 // Set up options for JWT Strategy
 jwtOptions = {
@@ -30,3 +63,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 
 // Tell passport to use this Strategy
 passport.use(jwtLogin);
+passport.use(localLogin);
